653d5df25db05e7b83e095efcba3fa1d27a82029
Grant Lemons <grantlemons@aol.com>
2025-09-05 15:13:46 -0600

    rewrite to use options and lists instead of custom writer monad

--- bin/main.ml
+++ bin/main.ml
@@ -1,56 +1,92 @@
-module Writer : sig
-  type 'a t = 'a * string
+type op = Add | Sub | Mul | Div | RSub | RDiv
 
-  val return : 'a -> 'a t
-  val join : 'a t -> 'b t -> 'b t
-  val bind : 'a t -> ('a -> 'b t) -> 'b t
-  val deref : 'a t -> 'a
-  val log : 'a t -> string
-end = struct
-  type 'a t = 'a * string
+let rec apply = function
+  | Add -> ( +. )
+  | Sub -> ( -. )
+  | Mul -> ( *. )
+  | Div -> ( /. )
+  | RSub -> fun a b -> apply Sub b a
+  | RDiv -> fun a b -> apply Div b a
 
-  let return x = (x, "")
-  let join (_, s1) (y, s2) = (y, s1 ^ s2)
-  let bind ((x, _) as m) f = join m @@ f x
-  let deref (a, _) = a
-  let log (_, b) = b
-end
+let inverse = function
+  | Add -> Sub
+  | Sub -> Add
+  | Mul -> Div
+  | Div -> Mul
+  | RSub -> Add
+  | RDiv -> Mul
 
-type op = Add | Sub | Mul | Div
+let op_to_string x y op =
+  let ( >>= ) = Option.bind in
+  x >>= fun a ->
+  y >>= fun b ->
+  Some
+    (match op with
+    | Add -> Printf.sprintf "(%s %s %s)" a "+" b
+    | Sub -> Printf.sprintf "(%s %s %s)" a "-" b
+    | Mul -> Printf.sprintf "(%s %s %s)" a "*" b
+    | Div -> Printf.sprintf "(%s %s %s)" a "/" b
+    | RSub -> Printf.sprintf "(%s %s %s)" b "-" a
+    | RDiv -> Printf.sprintf "(%s %s %s)" b "/" a)
 
-let apply = function
-  | Add -> ( + )
-  | Sub -> ( - )
-  | Mul -> ( * )
-  | Div ->
-      fun a b -> Int.to_float a /. Int.to_float b |> Float.round |> Float.to_int
+let nums = [ 1.; 3.; 4.; 7. ]
 
-let inverse = function Add -> Sub | Sub -> Add | Mul -> Div | Div -> Mul
-let to_string = function Add -> "+" | Sub -> "-" | Mul -> "*" | Div -> "/"
-let nums = [ 1; 3; 4; 7 ]
-let ops = [ Add; Sub; Mul; Div ]
-let ( >>= ) = Writer.bind
-let ( >+ ) = Writer.join
-let ( ! ) = Writer.deref
+(* RDiv and RSub are Div and Sub with swapped inputs *)
+let ops = [ Add; Sub; Mul; Div; RSub; RDiv ]
 
-let rec solve (nums : int list) v : bool Writer.t =
-  let other_input op = apply (inverse op) v in
-  let check op : bool Writer.t =
+(* Returns all possible combinations of operators and a list of two numbers *)
+let possible_outputs =
+  let module FloatSet = Set.Make (Float) in
+  function
+  | [ a; b ] ->
+      List.map (fun op -> apply op a b) ops
+      |> FloatSet.of_list |> FloatSet.to_list
+  | _ -> failwith "List not of two elements"
+
+(* Assumes len(nums) <= 4 *)
+let rec solve nums v : string list =
+  (* Recursive call cannot return a list because they cannot be on nums of length 4 *)
+  let solve nums v = List.nth_opt (solve nums v) 0 in
+  let check op =
+    let other_input = apply (inverse op) v in
+    let test l r = Option.is_some @@ solve l @@ other_input r in
+    let concat_known_other l r =
+      op_to_string (solve l @@ other_input r) (solve [ r ] r) op
+      |> Option.to_list
+    in
+    let test_unknown_other l r =
+      List.exists
+        (fun i -> Option.is_some @@ solve l @@ other_input i)
+        (possible_outputs r)
+    in
+    let concat_unknown_other l i =
+      op_to_string (solve l @@ other_input i) (Some (Float.to_string i)) op
+      |> Option.to_list
+    in
     match nums with
-    | [] -> failwith "Empty arr"
-    | [ x ] -> (x = v, Int.to_string x)
-    | x :: tl when !(solve tl @@ other_input op x) ->
-        solve [ x ] x
-        >+ (true, Printf.sprintf ")%s(" @@ to_string op)
-        >+ solve tl @@ other_input op x
-    | [ x; y; z ] when !(solve [ x; y ] @@ other_input op z) ->
-        solve [ z ] z
-        >+ (true, Printf.sprintf ")%s(" @@ to_string op)
-        >+ solve [ x; y ] @@ other_input op z
-    | [ x; y; z ] when !(solve [ x; z ] @@ other_input op y) ->
-        solve [ y ] y
-        >+ (true, Printf.sprintf ")%s(" @@ to_string op)
-        >+ solve [ x; z ] @@ other_input op y
-    | _ -> Writer.return false
+    | [] -> []
+    | [ x ] when x = v -> [ Float.to_string x ]
+    (*Split off first of any size array*)
+    | hd :: tl when test tl hd -> concat_known_other tl hd
+    (*Split off second of any size array*)
+    | a :: b :: tl when test (a :: tl) b -> concat_known_other (a :: tl) b
+    (*Split off third of any size array*)
+    | a :: b :: c :: tl when test (a :: b :: tl) c ->
+        concat_known_other (a :: b :: tl) c
+    (*Split off fourth of any size array*)
+    | a :: b :: c :: d :: tl when test (a :: b :: c :: tl) d ->
+        concat_known_other (a :: b :: c :: tl) d
+    | [ a; b; c; d ] when test_unknown_other [ a; b ] [ c; d ] ->
+        List.map (concat_unknown_other [ a; b ]) (possible_outputs [ c; d ])
+        |> List.flatten
+    | [ a; b; c; d ] when test_unknown_other [ a; d ] [ b; c ] ->
+        List.map (concat_unknown_other [ a; d ]) (possible_outputs [ b; c ])
+        |> List.flatten
+    | [ a; b; c; d ] when test_unknown_other [ a; c ] [ b; d ] ->
+        List.map (concat_unknown_other [ a; c ]) (possible_outputs [ b; d ])
+        |> List.flatten
+    | _ -> []
   in
-  List.map check ops |> List.hd
+  List.map check ops |> List.flatten
+
+let rsolve nums v = List.nth_opt (solve nums v) 0
